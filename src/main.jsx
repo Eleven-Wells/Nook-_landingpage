393b219f03963854e98efb8791251790c0cd8891
Abiodun oluwafunmibi <abiodunoluwafunmibi17@gmail.com>
2026-05-08 14:27:15 +0100

    cc

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,21 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
-)
+const root = document.getElementById('root')
+
+if (!root) {
+  document.body.innerHTML = '<h1 style="color:red">Root element not found</h1>'
+} else {
+  try {
+    createRoot(root).render(
+      <StrictMode>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </StrictMode>
+    )
+  } catch(e) {
+    document.body.innerHTML = '<h1 style="color:red">' + e.message + '</h1>'
+  }
+}
+  
